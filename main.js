97b196e0a7e3d6ebdfe8e0c0bff46d9f52b3381e
dunewalkin <sergei.gunchenko@gmail.com>
2023-10-09 15:50:37 +0400

    initial commit

--- main.js
+++ main.js
@@ -12,42 +12,11 @@ navToggle.addEventListener('click', ()  => {
       primaryNav.setAttribute ("data-visible", false);
       navToggle.setAttribute ("aria-expanded", false);
    }
-}); 
+});  
 
-// function toggleDropdown(button) {
-//    var dropdownContent = button.nextElementSibling; // Get the next sibling, which is the dropdown content
- 
-//    // Close all open dropdowns
-//    var allDropdowns = document.querySelectorAll(".dropdown-content");
-//    allDropdowns.forEach(function(dropdown) {
-//      if (dropdown !== dropdownContent && dropdown.style.display === "block") {
-//        dropdown.style.display = "none";
-//        // Remove the "active" class from the corresponding button
-//        var correspondingButton = dropdown.previousElementSibling;
-//        correspondingButton.classList.remove("active");
-//      }
-//    });
- 
-//    // Toggle the "active" class on the button
-//    button.classList.toggle("active");
- 
-//    // Toggle the display of the dropdown content
-//    if (dropdownContent.style.display === "block") {
-//      dropdownContent.style.display = "none";
-//    } else {
-//      dropdownContent.style.display = "block";
-//    }
-//  }
- 
- 
- 
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function toggleDropdown(button) {
    let dropdownContent = button.nextElementSibling;
    let isOpen = dropdownContent.classList.contains("show");
-  // Close all open dropdowns and remove the "active" class from all buttons
   closeAllDropdowns();
   removeActive();
 
@@ -57,7 +26,6 @@ function toggleDropdown(button) {
   }
 }
 
-// Close all open dropdowns
 function closeAllDropdowns() {
   for (let i = 0; i < dropdowns.length; i++) {
     let openDropdown = dropdowns[i];
@@ -67,7 +35,6 @@ function closeAllDropdowns() {
   }
 }
 
-// Remove the "active" class from all buttons
 function removeActive() {
   let buttons = document.getElementsByClassName("dropbtn");
   for (let i = 0; i < buttons.length; i++) {
@@ -76,9 +43,7 @@ function removeActive() {
   }
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-   // Check if the screen width is greater than or equal to 56.25em (900px)
    if (window.innerWidth >= 900) {
      if (!event.target.matches('.dropbtn')) {
        closeAllDropdowns();
